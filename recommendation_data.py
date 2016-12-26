b675e0b403041f868dc6e1b4f3d6883ab020ce32
Aryan Kashyap <matrixapp13@gmail.com>
2016-12-26 23:30:13 +0000

    Hi

---
+++ recommendation_data.py
@@ -0,0 +1,32 @@
+# Data set of people and movies they have rate 
+#0 means either they dont like it or they havent watched it yet.
+
+
+dataset={
+			'Alice': {'Love At Last': 5, 
+							'Romance Forever': 5,
+							'Cute puppies of love': 0, 
+							'Nonstop car chases': 0, 
+							'Swords vs karate': 0},
+
+			'Bob': {'Love At Last': 5, 
+							'Romance Forever': 0,
+							'Cute puppies of love': 4,
+							 'Nonstop car chases': 0,
+							  'Swords vs karate':0 
+							 },
+
+			'Carol': {'Love At Last': 0, 
+								'Romance Forever': 0,
+								'Cute puppies of love': 0,
+								 'Nonstop car chases': 5,
+								 'Swords vs karate':5,
+								 },
+
+
+
+			'Claudia Puig': {'Love At Last': 0, 
+							'Romance Forever': 0,
+							'Cute puppies of love': 0, 
+							'Nonstop car chases': 4,
+							'Swords vs karate': 0}}
